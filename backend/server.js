bbdda16f0d155f8f3b5342c5b85e302f7e76fc91
C0nstanzaL <colucero@alumnos.uai.cl>
2025-11-11 13:41:26 -0300

    creación backend y termine de configurar frontend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Endpoint de prueba
+app.get("/api/ping", (req, res) => {
+  res.json({ message: "API funcionando correctamente" });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
